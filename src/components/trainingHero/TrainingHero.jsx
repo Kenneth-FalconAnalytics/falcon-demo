4d67d638288d8afef80339454739e80cccf3160b
Kenneth Idisi <kennethidisi@falconanalytics.net>
2026-03-31 22:28:21 +0100

    test commit 2

--- src/components/trainingHero/TrainingHero.jsx
+++ src/components/trainingHero/TrainingHero.jsx
@@ -3,7 +3,6 @@ import Link from 'next/link'
 import React from 'react'
 import styles from './trainingHero.module.css'
 
-
 export default function TrainingHero() {
   return (
     <div className={styles.heroWrapper}>
